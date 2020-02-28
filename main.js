4ceb50f1862e4ccdc3c78246b392f53a82291f2c
Ewa Mitra <ewa.mitra@gmail.com>
2020-02-28 02:01:57 +0100

    feat: grid added

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ console.log(`Nazywam się ${name} i mam ${age}lat. Cieszę się, że zaglądasz 
 
 const knowMore = document.querySelector('.knowMore__extra--js');
 knowMore.innerHTML = `Nazywam się Ewa Mitra.Teraz muszę to napisać, a potem skasuję`;
+
+const welcome = (name, age)=> {
+    console.log(`Witaj, ${name}! Masz ${age} lat`);
+}
+welcome (name,age);
